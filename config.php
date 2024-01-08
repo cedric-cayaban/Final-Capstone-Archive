26cd696124f0e0171e59961bbcb0b8087cac2719
cedric <cedricjoelcayaban15@gmail.com>
2024-01-09 01:22:55 +0800

    updated prof home & groups

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "capstone_archive02";
+$database = "final_capstone_archive";
 $connect = new mysqli($host, $username, $password, $database);
