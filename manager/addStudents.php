dee0dac948a82f9a7fcb347016ad88967b3a8a90
John Doe <mac.jar16@gmail.com>
2024-01-09 11:31:16 +0800

    updated add stud

--- manager/addStudents.php
+++ manager/addStudents.php
@@ -63,7 +63,7 @@ if (isset($_SESSION['professorID']) && isset($_SESSION['profPassword'])) { ?>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../css files/add-students2.css">
+        <link rel="stylesheet" href="../css files/add-students3.css">
         <title>Document</title>
     </head>
 
