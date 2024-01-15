b9abbaa1ff4830aba4a872f4d071f704f7ce8eb7
Dan Galano <mooncoreee25@gmail.com>
2024-01-15 18:25:11 +0800

    my class

--- headers/user_header_home.php
+++ headers/user_header_home.php
@@ -1,17 +1,18 @@
-
 <?php
-        include "../config.php";
-        $userID = $_SESSION['userID'];
-        $sql = "SELECT * FROM user WHERE userID = '$userID'";
-        $result = mysqli_query($connect, $sql);
-        if ($result) {
-            // Fetch data
-            while ($row = $result->fetch_assoc()) {
-                // Access data using $row['column1'], $row['column2'], etc.
-                $username = $row['firstName'];
-            }} ?>
+include "../config.php";
+$userID = $_SESSION['userID'];
+$sql = "SELECT * FROM user WHERE userID = '$userID'";
+$result = mysqli_query($connect, $sql);
+if ($result) {
+    // Fetch data
+    while ($row = $result->fetch_assoc()) {
+        // Access data using $row['column1'], $row['column2'], etc.
+        $username = $row['firstName'];
+    }
+} ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,6 +23,7 @@
     <link rel="stylesheet" href="../css files/logout.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     if (isset($_POST['logout'])) {
@@ -35,21 +37,21 @@
             <img class="logo" src="../images/finalnlogo.svg" alt="PSU Logo" style="max-width: 300px; margin-right: 10px;">
             <!-- <label><b>CAPSTONE ARCHIVE</b></label> -->
         </div>
-       
+
         <form action="#" method="post" class="system-name">
-            <label for="" id="sys-name">Welcome, <?php echo $username;?>!</label>
-             <link rel="stylesheet" href="../css files/homepages9.css">
+            <label for="" id="sys-name">Welcome, <?php echo $username; ?>!</label>
+            <link rel="stylesheet" href="../css files/homepages9.css">
             <!-- <button type="submit" name="logout" id="logout" class="btn">
                 <img src="../images/power.png" style="width: 40px; border-radius: 50px; border: none;" alt="Logout">
             </button> -->
-            <button type="submit"  name="logout" id="logout" class="new-button" >
-                            <img style= "width: 25px;
+            <button type="submit" name="logout" id="logout" class="new-button">
+                <img style="width: 25px;
                         border-radius: 0px;
-                        float: left;"src="../images/logout_icon.png"alt="Logout">
-                        
-                    <div class="new-logout">LOGOUT</div>
+                        float: left;" src="../images/logout_icon.png" alt="Logout">
 
-</button >
+                <div class="new-logout">LOGOUT</div>
+
+            </button>
         </form>
     </header>
 
@@ -58,7 +60,7 @@
             <div class="collapse navbar-collapse" id="navi">
 
                 <ul class="navbar-nav mb-2 mb-lg-0" id="left-nav">
-                    
+
                     <li class="nav-item">
                         <a href="user-home.php" class="nav-link active">Home</a>
                     </li>
@@ -66,30 +68,36 @@
                         <a href="checkers.php" class="nav-link">Checker</a>
                     </li>
                 </ul>
-                    <?php
-        include "../config.php";
-        $userID = $_SESSION['userID'];
-        $sql = "SELECT * FROM groups WHERE leaderID = '$userID'";
-        $result = mysqli_query($connect, $sql);
-        if ($row = mysqli_num_rows($result) > 0) { ?>
                 <ul class="navbar-nav" id="right-nav">
                     <li class="nav-item">
-                        <a href="uploads.php" class="nav-link">
-                            &nbsp;&nbsp;Project
+                        <a href="viewClass.php" class="nav-link">
+                            &nbsp;&nbsp;My Class
                         </a>
                     </li>
+                    <?php
+                    include "../config.php";
+                    $userID = $_SESSION['userID'];
+                    $sql = "SELECT * FROM groups WHERE leaderID = '$userID'";
+                    $result = mysqli_query($connect, $sql);
+                    if ($row = mysqli_num_rows($result) > 0) { ?>
+                        <li class="nav-item">
+                            <a href="uploads.php" class="nav-link">
+                                &nbsp;&nbsp;Project
+                            </a>
+                        </li>
                 </ul>
-                <?php } ?>
+            <?php } ?>
 
 
             </div>
         </div>
     </nav>
-    
+
 
     <!-- Add your content here -->
 
     <!-- Bootstrap JS and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
